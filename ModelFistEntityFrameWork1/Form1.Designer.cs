da67b6194c1431c11974537f12c727a1a3bfed86
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-06-22 21:36:40 +0300

    Eğitim Ders 2
    Ekleme işlemleri yapıldı.

--- ModelFistEntityFrameWork1/Form1.Designer.cs
+++ ModelFistEntityFrameWork1/Form1.Designer.cs
@@ -33,14 +33,14 @@ namespace ModelFistEntityFrameWork1
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.pictureEdit1 = new DevExpress.XtraEditors.PictureEdit();
             this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
-            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
-            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.TeID = new DevExpress.XtraEditors.TextEdit();
+            this.TeAd = new DevExpress.XtraEditors.TextEdit();
             this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
-            this.textEdit3 = new DevExpress.XtraEditors.TextEdit();
+            this.TeSoyad = new DevExpress.XtraEditors.TextEdit();
             this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
-            this.textEdit4 = new DevExpress.XtraEditors.TextEdit();
+            this.TeSehir = new DevExpress.XtraEditors.TextEdit();
             this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
-            this.textEdit5 = new DevExpress.XtraEditors.TextEdit();
+            this.TeMaas = new DevExpress.XtraEditors.TextEdit();
             this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
             this.SBtnEkle = new DevExpress.XtraEditors.SimpleButton();
             this.SBtnListele = new DevExpress.XtraEditors.SimpleButton();
@@ -50,11 +50,11 @@ namespace ModelFistEntityFrameWork1
             this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
             this.pictureEdit2 = new DevExpress.XtraEditors.PictureEdit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeID.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeAd.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeSoyad.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeSehir.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeMaas.Properties)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit2.Properties)).BeginInit();
@@ -73,7 +73,7 @@ namespace ModelFistEntityFrameWork1
             // pictureEdit1
             // 
             this.pictureEdit1.EditValue = ((object)(resources.GetObject("pictureEdit1.EditValue")));
-            this.pictureEdit1.Location = new System.Drawing.Point(305, 12);
+            this.pictureEdit1.Location = new System.Drawing.Point(305, 17);
             this.pictureEdit1.Name = "pictureEdit1";
             this.pictureEdit1.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
             this.pictureEdit1.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
@@ -84,30 +84,30 @@ namespace ModelFistEntityFrameWork1
             // 
             this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.labelControl2.Appearance.Options.UseFont = true;
-            this.labelControl2.Location = new System.Drawing.Point(368, 20);
+            this.labelControl2.Location = new System.Drawing.Point(368, 25);
             this.labelControl2.Name = "labelControl2";
             this.labelControl2.Size = new System.Drawing.Size(210, 33);
             this.labelControl2.TabIndex = 2;
             this.labelControl2.Text = "Personel İşlemleri";
             this.labelControl2.Click += new System.EventHandler(this.labelControl2_Click);
             // 
-            // textEdit1
+            // TeID
             // 
-            this.textEdit1.Location = new System.Drawing.Point(88, 89);
-            this.textEdit1.Name = "textEdit1";
-            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textEdit1.Properties.Appearance.Options.UseFont = true;
-            this.textEdit1.Size = new System.Drawing.Size(185, 22);
-            this.textEdit1.TabIndex = 3;
+            this.TeID.Location = new System.Drawing.Point(88, 89);
+            this.TeID.Name = "TeID";
+            this.TeID.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TeID.Properties.Appearance.Options.UseFont = true;
+            this.TeID.Size = new System.Drawing.Size(185, 22);
+            this.TeID.TabIndex = 3;
             // 
-            // textEdit2
+            // TeAd
             // 
-            this.textEdit2.Location = new System.Drawing.Point(88, 117);
-            this.textEdit2.Name = "textEdit2";
-            this.textEdit2.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textEdit2.Properties.Appearance.Options.UseFont = true;
-            this.textEdit2.Size = new System.Drawing.Size(185, 22);
-            this.textEdit2.TabIndex = 5;
+            this.TeAd.Location = new System.Drawing.Point(88, 117);
+            this.TeAd.Name = "TeAd";
+            this.TeAd.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TeAd.Properties.Appearance.Options.UseFont = true;
+            this.TeAd.Size = new System.Drawing.Size(185, 22);
+            this.TeAd.TabIndex = 5;
             // 
             // labelControl3
             // 
@@ -119,14 +119,14 @@ namespace ModelFistEntityFrameWork1
             this.labelControl3.TabIndex = 4;
             this.labelControl3.Text = "Ad";
             // 
-            // textEdit3
+            // TeSoyad
             // 
-            this.textEdit3.Location = new System.Drawing.Point(88, 145);
-            this.textEdit3.Name = "textEdit3";
-            this.textEdit3.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textEdit3.Properties.Appearance.Options.UseFont = true;
-            this.textEdit3.Size = new System.Drawing.Size(185, 22);
-            this.textEdit3.TabIndex = 7;
+            this.TeSoyad.Location = new System.Drawing.Point(88, 145);
+            this.TeSoyad.Name = "TeSoyad";
+            this.TeSoyad.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TeSoyad.Properties.Appearance.Options.UseFont = true;
+            this.TeSoyad.Size = new System.Drawing.Size(185, 22);
+            this.TeSoyad.TabIndex = 7;
             // 
             // labelControl4
             // 
@@ -138,14 +138,14 @@ namespace ModelFistEntityFrameWork1
             this.labelControl4.TabIndex = 6;
             this.labelControl4.Text = "Soyad:";
             // 
-            // textEdit4
+            // TeSehir
             // 
-            this.textEdit4.Location = new System.Drawing.Point(88, 173);
-            this.textEdit4.Name = "textEdit4";
-            this.textEdit4.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textEdit4.Properties.Appearance.Options.UseFont = true;
-            this.textEdit4.Size = new System.Drawing.Size(185, 22);
-            this.textEdit4.TabIndex = 9;
+            this.TeSehir.Location = new System.Drawing.Point(88, 173);
+            this.TeSehir.Name = "TeSehir";
+            this.TeSehir.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TeSehir.Properties.Appearance.Options.UseFont = true;
+            this.TeSehir.Size = new System.Drawing.Size(185, 22);
+            this.TeSehir.TabIndex = 9;
             // 
             // labelControl5
             // 
@@ -157,14 +157,14 @@ namespace ModelFistEntityFrameWork1
             this.labelControl5.TabIndex = 8;
             this.labelControl5.Text = "Şehir:";
             // 
-            // textEdit5
+            // TeMaas
             // 
-            this.textEdit5.Location = new System.Drawing.Point(88, 201);
-            this.textEdit5.Name = "textEdit5";
-            this.textEdit5.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.textEdit5.Properties.Appearance.Options.UseFont = true;
-            this.textEdit5.Size = new System.Drawing.Size(185, 22);
-            this.textEdit5.TabIndex = 11;
+            this.TeMaas.Location = new System.Drawing.Point(88, 201);
+            this.TeMaas.Name = "TeMaas";
+            this.TeMaas.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.TeMaas.Properties.Appearance.Options.UseFont = true;
+            this.TeMaas.Size = new System.Drawing.Size(185, 22);
+            this.TeMaas.TabIndex = 11;
             // 
             // labelControl6
             // 
@@ -186,6 +186,7 @@ namespace ModelFistEntityFrameWork1
             this.SBtnEkle.Size = new System.Drawing.Size(261, 37);
             this.SBtnEkle.TabIndex = 12;
             this.SBtnEkle.Text = "Ekle";
+            this.SBtnEkle.Click += new System.EventHandler(this.SBtnEkle_Click);
             // 
             // SBtnListele
             // 
@@ -239,7 +240,7 @@ namespace ModelFistEntityFrameWork1
             // pictureEdit2
             // 
             this.pictureEdit2.EditValue = ((object)(resources.GetObject("pictureEdit2.EditValue")));
-            this.pictureEdit2.Location = new System.Drawing.Point(584, 12);
+            this.pictureEdit2.Location = new System.Drawing.Point(584, 17);
             this.pictureEdit2.Name = "pictureEdit2";
             this.pictureEdit2.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
             this.pictureEdit2.Properties.SizeMode = DevExpress.XtraEditors.Controls.PictureSizeMode.Stretch;
@@ -257,15 +258,15 @@ namespace ModelFistEntityFrameWork1
             this.Controls.Add(this.SBtnSil);
             this.Controls.Add(this.SBtnListele);
             this.Controls.Add(this.SBtnEkle);
-            this.Controls.Add(this.textEdit5);
+            this.Controls.Add(this.TeMaas);
             this.Controls.Add(this.labelControl6);
-            this.Controls.Add(this.textEdit4);
+            this.Controls.Add(this.TeSehir);
             this.Controls.Add(this.labelControl5);
-            this.Controls.Add(this.textEdit3);
+            this.Controls.Add(this.TeSoyad);
             this.Controls.Add(this.labelControl4);
-            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.TeAd);
             this.Controls.Add(this.labelControl3);
-            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.TeID);
             this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.pictureEdit1);
             this.Controls.Add(this.labelControl1);
@@ -273,11 +274,11 @@ namespace ModelFistEntityFrameWork1
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeID.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeAd.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeSoyad.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeSehir.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TeMaas.Properties)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureEdit2.Properties)).EndInit();
@@ -291,14 +292,14 @@ namespace ModelFistEntityFrameWork1
         private DevExpress.XtraEditors.LabelControl labelControl1;
         private DevExpress.XtraEditors.PictureEdit pictureEdit1;
         private DevExpress.XtraEditors.LabelControl labelControl2;
-        private DevExpress.XtraEditors.TextEdit textEdit1;
-        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.TextEdit TeID;
+        private DevExpress.XtraEditors.TextEdit TeAd;
         private DevExpress.XtraEditors.LabelControl labelControl3;
-        private DevExpress.XtraEditors.TextEdit textEdit3;
+        private DevExpress.XtraEditors.TextEdit TeSoyad;
         private DevExpress.XtraEditors.LabelControl labelControl4;
-        private DevExpress.XtraEditors.TextEdit textEdit4;
+        private DevExpress.XtraEditors.TextEdit TeSehir;
         private DevExpress.XtraEditors.LabelControl labelControl5;
-        private DevExpress.XtraEditors.TextEdit textEdit5;
+        private DevExpress.XtraEditors.TextEdit TeMaas;
         private DevExpress.XtraEditors.LabelControl labelControl6;
         private DevExpress.XtraEditors.SimpleButton SBtnEkle;
         private DevExpress.XtraEditors.SimpleButton SBtnListele;
--- ModelFistEntityFrameWork1/Form1.cs
+++ ModelFistEntityFrameWork1/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DevExpress.XtraEditors;
 
 namespace ModelFistEntityFrameWork1
 {
@@ -24,6 +25,19 @@ namespace ModelFistEntityFrameWork1
         PersonelEntities ent = new PersonelEntities();
         private void Form1_Load(object sender, EventArgs e)
         {
+            gridControl1.DataSource = ent.TblPersonel.ToList();
+        }
+
+        private void SBtnEkle_Click(object sender, EventArgs e)
+        {
+            TblPersonel Tablo = new TblPersonel();
+            Tablo.PerAd = TeAd.Text;
+            Tablo.PerSoyad = TeSoyad.Text;
+            Tablo.PerSehir = TeSehir.Text;
+            Tablo.PerMaas = Convert.ToInt16(TeMaas.Text);
+            ent.TblPersonel.Add(Tablo);
+            ent.SaveChanges();
+            XtraMessageBox.Show("Personel Başarılı","Personel",MessageBoxButtons.OK,MessageBoxIcon.Information);
             gridControl1.DataSource = ent.TblPersonel.ToList();
         }
     }
