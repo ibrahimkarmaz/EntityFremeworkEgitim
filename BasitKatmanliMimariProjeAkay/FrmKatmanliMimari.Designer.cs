3c3d457fbed3987dd1a00a184d55d9b9e9f1564c
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-06-25 23:13:01 +0300

    GridControl İşlemleri ve Arama
    GridControl üzerinde arama yapıldı getList

--- BasitKatmanliMimariProjeAkay/FrmKatmanliMimari.Designer.cs
+++ BasitKatmanliMimariProjeAkay/FrmKatmanliMimari.Designer.cs
@@ -42,9 +42,30 @@ namespace BasitKatmanliMimariProjeAkay
             this.BirimID = new DevExpress.XtraGrid.Columns.GridColumn();
             this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
             this.TxeAramaKonusu = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit3 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit4 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit5 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit6 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit7 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl8 = new DevExpress.XtraEditors.LabelControl();
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.TxeAramaKonusu.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit7.Properties)).BeginInit();
             this.SuspendLayout();
             // 
             // gridControl1
@@ -55,7 +76,7 @@ namespace BasitKatmanliMimariProjeAkay
             this.gridControl1.Location = new System.Drawing.Point(2, 35);
             this.gridControl1.MainView = this.gridView1;
             this.gridControl1.Name = "gridControl1";
-            this.gridControl1.Size = new System.Drawing.Size(783, 295);
+            this.gridControl1.Size = new System.Drawing.Size(783, 271);
             this.gridControl1.TabIndex = 0;
             this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridView1});
@@ -164,12 +185,131 @@ namespace BasitKatmanliMimariProjeAkay
             this.TxeAramaKonusu.TabIndex = 2;
             this.TxeAramaKonusu.EditValueChanged += new System.EventHandler(this.TxeAramaKonusu_EditValueChanged);
             // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(83, 312);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(176, 20);
+            this.textEdit1.TabIndex = 4;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 315);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(65, 13);
+            this.labelControl2.TabIndex = 3;
+            this.labelControl2.Text = "Personel Ara:";
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(83, 338);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(176, 20);
+            this.textEdit2.TabIndex = 6;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(12, 341);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(65, 13);
+            this.labelControl3.TabIndex = 5;
+            this.labelControl3.Text = "Personel Ara:";
+            // 
+            // textEdit3
+            // 
+            this.textEdit3.Location = new System.Drawing.Point(83, 364);
+            this.textEdit3.Name = "textEdit3";
+            this.textEdit3.Size = new System.Drawing.Size(176, 20);
+            this.textEdit3.TabIndex = 8;
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(12, 367);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(65, 13);
+            this.labelControl4.TabIndex = 7;
+            this.labelControl4.Text = "Personel Ara:";
+            // 
+            // textEdit4
+            // 
+            this.textEdit4.Location = new System.Drawing.Point(83, 390);
+            this.textEdit4.Name = "textEdit4";
+            this.textEdit4.Size = new System.Drawing.Size(176, 20);
+            this.textEdit4.TabIndex = 10;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(12, 393);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(65, 13);
+            this.labelControl5.TabIndex = 9;
+            this.labelControl5.Text = "Personel Ara:";
+            // 
+            // textEdit5
+            // 
+            this.textEdit5.Location = new System.Drawing.Point(83, 416);
+            this.textEdit5.Name = "textEdit5";
+            this.textEdit5.Size = new System.Drawing.Size(176, 20);
+            this.textEdit5.TabIndex = 12;
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(12, 419);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(65, 13);
+            this.labelControl6.TabIndex = 11;
+            this.labelControl6.Text = "Personel Ara:";
+            // 
+            // textEdit6
+            // 
+            this.textEdit6.Location = new System.Drawing.Point(83, 442);
+            this.textEdit6.Name = "textEdit6";
+            this.textEdit6.Size = new System.Drawing.Size(176, 20);
+            this.textEdit6.TabIndex = 14;
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Location = new System.Drawing.Point(12, 445);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(65, 13);
+            this.labelControl7.TabIndex = 13;
+            this.labelControl7.Text = "Personel Ara:";
+            // 
+            // textEdit7
+            // 
+            this.textEdit7.Location = new System.Drawing.Point(83, 468);
+            this.textEdit7.Name = "textEdit7";
+            this.textEdit7.Size = new System.Drawing.Size(176, 20);
+            this.textEdit7.TabIndex = 16;
+            // 
+            // labelControl8
+            // 
+            this.labelControl8.Location = new System.Drawing.Point(12, 471);
+            this.labelControl8.Name = "labelControl8";
+            this.labelControl8.Size = new System.Drawing.Size(65, 13);
+            this.labelControl8.TabIndex = 15;
+            this.labelControl8.Text = "Personel Ara:";
+            // 
             // FrmKatmanliMimari
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(782, 329);
+            this.ClientSize = new System.Drawing.Size(782, 510);
+            this.Controls.Add(this.textEdit7);
+            this.Controls.Add(this.labelControl8);
+            this.Controls.Add(this.textEdit6);
+            this.Controls.Add(this.labelControl7);
+            this.Controls.Add(this.textEdit5);
+            this.Controls.Add(this.labelControl6);
+            this.Controls.Add(this.textEdit4);
+            this.Controls.Add(this.labelControl5);
+            this.Controls.Add(this.textEdit3);
+            this.Controls.Add(this.labelControl4);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.labelControl2);
             this.Controls.Add(this.TxeAramaKonusu);
             this.Controls.Add(this.labelControl1);
             this.Controls.Add(this.gridControl1);
@@ -180,6 +320,13 @@ namespace BasitKatmanliMimariProjeAkay
             ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.TxeAramaKonusu.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit4.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit5.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit6.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit7.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -200,6 +347,20 @@ namespace BasitKatmanliMimariProjeAkay
         private DevExpress.XtraGrid.Columns.GridColumn IsActive;
         private DevExpress.XtraGrid.Columns.GridColumn BirimID;
         private DevExpress.XtraGrid.Columns.GridColumn Email;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private DevExpress.XtraEditors.TextEdit textEdit3;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.TextEdit textEdit4;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.TextEdit textEdit5;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.TextEdit textEdit6;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.TextEdit textEdit7;
+        private DevExpress.XtraEditors.LabelControl labelControl8;
     }
 }
 
