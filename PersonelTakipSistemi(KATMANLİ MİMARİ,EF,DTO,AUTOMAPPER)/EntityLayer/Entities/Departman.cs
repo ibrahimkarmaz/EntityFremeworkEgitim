4f8cc33e96a58425e821fa57ff622dfa964adcda
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-07-29 22:52:25 +0300

    KATMANLİ MİMARİ+EF+DTO+AUTOMAPPER

---
+++ PersonelTakipSistemi(KATMANLİ MİMARİ,EF,DTO,AUTOMAPPER)/EntityLayer/Entities/Departman.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Departman
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(50)]
+        public string DepartmanAd { get; set; }
+
+        public ICollection<Personel> Personel { get; set; }
+    }
+}
---
+++ PersonelTakipSistemi(KATMANLİ MİMARİ,EF,DTO,AUTOMAPPER)/EntityLayer/Entities/Personel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Entities
+{
+    public class Personel
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(20)]
+        public string Ad { get; set; }
+        [StringLength(20)]
+        public string Soyad { get; set; }
+        public bool Cinsiyet { get; set; }
+        public bool Arsiv { get; set; }
+
+        public int DepartmanID { get; set; }
+        public virtual Departman Departmans { get; set; }
+    }
+}
