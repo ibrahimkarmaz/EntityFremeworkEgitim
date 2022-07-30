ccaf0b28a1d11e9d6f72bc6a6b1f4c3be4e95c21
İbrahim KARMAZ <107946057+ibrahimkarmaz@users.noreply.github.com>
2022-07-30 17:28:55 +0300

    LinqSorgulari Restriction,Select Kullanımı, SelectMany,Take,Skip,ThenBy
    .

---
+++ EFLinqSorgulariRestrictionKisitlama/EFLinqSorgulariRestrictionKisitlama/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿
+namespace EFLinqSorgulariRestrictionKisitlama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gridControl1.Location = new System.Drawing.Point(12, 12);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(776, 255);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(12, 273);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(197, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Ad Küçük Soyad Büyük Olarak Getir";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(12, 401);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(776, 37);
+            this.simpleButton1.TabIndex = 2;
+            this.simpleButton1.Text = "LİNQ Entity";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(583, 273);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(205, 20);
+            this.textEdit1.TabIndex = 3;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(12, 296);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(106, 17);
+            this.radioButton2.TabIndex = 4;
+            this.radioButton2.Text = "Aktif Mi Basit Mi?";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(12, 319);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(199, 17);
+            this.radioButton3.TabIndex = 5;
+            this.radioButton3.Text = "Departmani Büyük Karakterlerle Getir";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(12, 342);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(58, 17);
+            this.radioButton4.TabIndex = 6;
+            this.radioButton4.Text = "Birleştir";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(12, 365);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(77, 17);
+            this.radioButton5.TabIndex = 7;
+            this.radioButton5.Text = "İlk 3 Değer";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(225, 274);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(83, 17);
+            this.radioButton6.TabIndex = 8;
+            this.radioButton6.Text = "Son 5 değer";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(225, 296);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(99, 17);
+            this.radioButton7.TabIndex = 11;
+            this.radioButton7.Text = "Ada Göre Sırala";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(225, 318);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(100, 17);
+            this.radioButton8.TabIndex = 10;
+            this.radioButton8.Text = "İlk 5 Değeri Atla";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(225, 341);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(161, 17);
+            this.radioButton9.TabIndex = 9;
+            this.radioButton9.Text = "Soyad ve Ada Göre Sıralama";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(710, 378);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(78, 17);
+            this.radioButton10.TabIndex = 12;
+            this.radioButton10.Text = "Bütün Liste";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.gridControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+    }
+}
+
---
+++ EFLinqSorgulariRestrictionKisitlama/EFLinqSorgulariRestrictionKisitlama/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFLinqSorgulariRestrictionKisitlama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        DbIsTakipEntities dataBase = new DbIsTakipEntities();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // GetAll();
+        }
+        private void GetAll()
+        {
+
+            gridControl1.DataSource = (from x in dataBase.TblPersonels
+                                       select new
+                                       {
+                                           x.ID,
+                                           x.Ad,
+                                           x.Soyad,
+                                           x.Mail,
+                                       }).ToList();
+        }
+
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                gridControl1.DataSource = (from x in dataBase.TblPersonels
+                                           select new
+                                           {
+                                               x.ID,
+                                               Ad = x.Ad.Substring(0, 1).ToUpper() + x.Ad.Substring(1).ToLower(),
+                                               Soyad = x.Soyad.ToUpper(),
+                                               x.Mail,
+                                           }).ToList();
+            }
+
+            if (radioButton2.Checked)
+            {
+                gridControl1.DataSource = (from x in dataBase.TblPersonels
+                                           select new
+                                           {
+                                               x.ID,
+                                               Ad = x.Ad.Substring(0, 1).ToUpper() + x.Ad.Substring(1).ToLower(),
+                                               Soyad = x.Soyad.ToUpper(),
+                                               x.Mail,
+                                               Durum = x.Arsiv==true ? "AKTİF":"PASİF"
+                                           }).ToList();
+            }
+
+            if (radioButton3.Checked)
+            {//Select Kullanımları
+                gridControl1.DataSource = dataBase.TblPersonels.Select(x => new { DepartmanAd = x.TblDepartmanlar.Ad }).ToList();
+            }
+
+            if (radioButton4.Checked)
+            {//SelectMany 
+                gridControl1.DataSource = (dataBase.TblPersonels.SelectMany(x => dataBase.TblDepartmanlars.Where(y => y.ID == x.DepartmanID), (x, y) => new
+                {
+                    AdSoyad = x.Ad + " " + x.Soyad,
+                    DepartmanAd = y.Ad
+                })).ToList();
+            }
+
+
+
+            if (radioButton5.Checked)
+            { //Take:sql top kodu
+                gridControl1.DataSource = (dataBase.TblPersonels.OrderBy(x=>x.ID).Take(3).ToList());
+            } 
+            
+            if (radioButton6.Checked)
+            { //TersSırala
+                gridControl1.DataSource = (dataBase.TblPersonels.OrderByDescending(x=>x.ID).Take(5).ToList());
+            }
+            if (radioButton7.Checked)
+            { //OrderBy
+                gridControl1.DataSource = (dataBase.TblPersonels.OrderBy(x => x.Ad).ToList());
+            }
+            if (radioButton8.Checked)
+            { //Skip:Atlama yapar
+                gridControl1.DataSource = (dataBase.TblPersonels.OrderBy(x => x.ID).Skip(5).ToList());
+            }
+            if (radioButton9.Checked)
+            { //Then By:Order bydan sonra işlemi alınan yeni siralama türü (Her zaman Order By 1. Sıralamayı yapar sonra 2.Sıralamayı Then By yapar)
+                gridControl1.DataSource = (dataBase.TblPersonels.OrderBy(x => x.Soyad).ThenBy(y=>y.Ad).ToList());
+            }//Ekstra:ThenByDescending:2.Ters Sıralama
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            if (radioButton10.Checked)
+            {
+                GetAll();
+            }
+        }
+    }
+}
